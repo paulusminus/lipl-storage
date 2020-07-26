a43c75fb547d9706ac44913f484a8965fea44387
Paul Min <info@paulmin.nl>
2020-07-26 18:21:09 +0200

    Refactor. Replacing matches.

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,20 @@ impl Store {
             lyric_list:  Arc::new(RwLock::new(get_lyrics_store())),
         }
     }
+    fn get_summaries(&self) -> Vec<LyricSummary> {
+        let mut result: Vec<LyricSummary> = vec!();
+        for (id, lyric) in self.lyric_list.read().iter() {
+            result.push(LyricSummary { id: id.clone(), title: format!("{}", lyric.title.to_string_lossy()) });
+        }
+        result
+    }
+    fn get_lyric(&self, id: i32) -> Option<Lyric> {
+        self.lyric_list.read().get(&id).map(|l| Lyric {
+            title: l.title.to_string_lossy().to_string(), 
+            parts: l.parts.clone(), 
+            id: id,
+        })
+    }
 }
 
 fn get_lyrics_store() -> Lyrics {
@@ -38,7 +52,6 @@ fn get_lyrics_store() -> Lyrics {
     let mut count = 0;
 
     let entries = lipl::get_songs("/home/paul/Documenten/lipl.data/Geheugenkoor", "txt").unwrap();
-
     for entry in entries {
         match entry {
             Ok(e) => { 
@@ -52,35 +65,25 @@ fn get_lyrics_store() -> Lyrics {
     result
 }
 
+fn lyric_id_from_path(path: String) -> i32 {
+    path.parse::<i32>().unwrap_or_default()
+}
 
 async fn get_lyric_list(store: Store) -> Result<impl Reply, Rejection> {
-    let mut result: Vec<LyricSummary> = vec!();
-    let r = store.lyric_list.read();
-
-    for (id, lyric) in r.iter() {
-        result.push(LyricSummary { id: id.clone(), title: format!("{}", lyric.title.to_string_lossy()) });
-    }
-
     Ok(warp::reply::json(
-        &result,
+        &store.get_summaries()
     ))
 }
 
 async fn get_lyric(path: String, store: Store) -> Result<impl Reply, Rejection> {
-    let lyric_id = path.parse::<i32>().unwrap_or_default();
-    
-    match store.lyric_list.read().get(&lyric_id) {
-        Some(item) => Ok(
-            warp::reply::json(
-                &Lyric { 
-                    title: item.title.to_string_lossy().to_string(), 
-                    parts: item.parts.clone(), 
-                    id: lyric_id 
-                }
-            )
-        ),
-        None => Err(warp::reject::not_found()),
-    }
+    store
+    .get_lyric(
+        lyric_id_from_path(path)
+    )
+    .map_or_else(
+        || Err(warp::reject::not_found()),
+        |lyric| Ok(warp::reply::json(&lyric)),
+    )
 }
 
 #[tokio::main]
@@ -88,14 +91,16 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let store = Store::new();
     let store_filter = warp::any().map(move || store.clone());
 
-    let get_items = warp::get()
+    let get_items = 
+        warp::get()
         .and(warp::path("v1"))
         .and(warp::path("lyric"))
         .and(warp::path::end())
         .and(store_filter.clone())
         .and_then(get_lyric_list);
 
-    let get_item = warp::get()
+    let get_item = 
+        warp::get()
         .and(warp::path("v1"))
         .and(warp::path("lyric"))
         .and(warp::path::param())
@@ -103,8 +108,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .and_then(get_lyric);
 
 
-    let routes = get_items
-    .or(get_item);
+    let routes = 
+        get_items
+        .or(get_item);
 
     warp::serve(routes)
         .run(([127, 0, 0, 1], 3030))
