789f5ce26268be8213aa4e7af92e6dbfb4775b25
Paul Min <info@paulmin.nl>
2020-10-31 16:05:57 +0100

    Show elapsed time in example.

--- examples/01.rs
+++ examples/01.rs
@@ -1,22 +1,26 @@
 use std::error::Error;
 use lipl_io::{get_lyrics};
 use tokio::stream::StreamExt;
+use std::time::{Instant};
 
 const DIR: &str = "/home/paul/Code/rust/filesystem/data/";
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
+    let start = Instant::now();
+
     let result = get_lyrics(DIR).await?;
     tokio::pin!(result);
 
     while let Some(lyric) = result.next().await {
-        println!("{}", lyric.id);
+        println!("id: {}", lyric.id);
         println!("Number of parts: {}", lyric.parts.len());
         
-        if lyric.yaml.is_some() {
-            println!("{}", lyric.yaml.unwrap());
+        if let Some(yaml) = lyric.yaml {
+            println!("{}", yaml);
         }
     }
 
+    println!("Elapsed: {} ms", start.elapsed().as_millis());
     Ok(())
 }
