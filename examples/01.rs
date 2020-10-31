7ee43b3e742cf26ee06c394bedffbe7e702fad85
Paul Min <info@paulmin.nl>
2020-10-31 20:11:51 +0100

    Example directory argument + error handling

--- examples/01.rs
+++ examples/01.rs
@@ -1,22 +1,30 @@
-use std::error::Error;
+use std::io::{ErrorKind};
 use lipl_io::{get_lyrics};
 use tokio::stream::StreamExt;
 use std::time::{Instant};
 use serde::{Deserialize, Serialize};
 
-const DIR: &str = "/home/paul/Code/rust/filesystem/data/";
-
 #[derive(Debug, PartialEq, Serialize, Deserialize)]
-struct Whatever {
+struct Frontmatter {
     title: Option<String>,
     member_of: Option<Vec<String>>
 }
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() -> Result<(), std::io::Error> {
     let start = Instant::now();
 
-    let result = get_lyrics(DIR).await?;
+    let mut args = std::env::args();
+    if args.len() < 2 {
+        return Err(std::io::Error::new(ErrorKind::Other, "Argument directory missing"));
+    }
+
+    let path = args.nth(1).unwrap();
+    if !std::path::Path::new(&path).exists() {
+        return Err(std::io::Error::new(ErrorKind::Other, "Directory not found"));
+    }
+
+    let result = get_lyrics(&path).await.expect(&format!("No results for {}", path));
     tokio::pin!(result);
 
     while let Some(lyric) = result.next().await {
@@ -24,11 +32,11 @@ async fn main() -> Result<(), Box<dyn Error>> {
         println!("Number of parts: {}", lyric.parts.len());
      
         if let Some(yaml) = lyric.yaml {
-            let whatever: Whatever = serde_yaml::from_str(&yaml).unwrap();
-            if let Some(title) = whatever.title {
+            let frontmatter: Frontmatter = serde_yaml::from_str(&yaml).unwrap();
+            if let Some(title) = frontmatter.title {
                 println!("Title: {}", title);
             }
-            if let Some(member_of) = whatever.member_of {
+            if let Some(member_of) = frontmatter.member_of {
                 println!("Member of: {}", member_of.join(", "));
             }
         }
