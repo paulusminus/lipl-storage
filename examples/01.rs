7faaaec69b46d03571d71b13aaced79e4ba89d18
Paul Min <info@paulmin.nl>
2020-11-03 22:06:09 +0100

    Use single thread.

--- examples/01.rs
+++ examples/01.rs
@@ -1,38 +1,43 @@
 use std::io::{ErrorKind};
 use lipl_io::{get_lyrics};
+use tokio::runtime::{Builder};
 use tokio::stream::StreamExt;
 use std::time::{Instant};
 
-#[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
-    let start = Instant::now();
+fn main() -> Result<(), std::io::Error> {
 
-    let mut args = std::env::args();
-    if args.len() < 2 {
-        return Err(std::io::Error::new(ErrorKind::Other, "Argument directory missing"));
-    }
+    let rt = Builder::new_current_thread().enable_all().build().unwrap();
 
-    let path = args.nth(1).unwrap();
-    if !std::path::Path::new(&path).exists() {
-        return Err(std::io::Error::new(ErrorKind::Other, "Directory not found"));
-    }
+    rt.block_on(async {
+        let start = Instant::now();
 
-    let result = get_lyrics(&path).await.expect(&format!("No results for {}", path));
-    tokio::pin!(result);
-
-    while let Some(lyric) = result.next().await {
-        println!("id: {}", lyric.id);
-        println!("Number of parts: {}", lyric.parts.len());
-     
-        if let Some(title) = lyric.title {
-            println!("Title: {}", title);
+        let mut args = std::env::args();
+        if args.len() < 2 {
+            return Err(std::io::Error::new(ErrorKind::Other, "Argument directory missing"));
         }
-        if let Some(member_of) = lyric.member_of {
-            println!("Member of: {}", member_of.join(", "));
+    
+        let path = args.nth(1).unwrap();
+        if !std::path::Path::new(&path).exists() {
+            return Err(std::io::Error::new(ErrorKind::Other, "Directory not found"));
         }
-        println!();
-    }
-
-    println!("Elapsed: {} ms", start.elapsed().as_millis());
-    Ok(())
+    
+        let result = get_lyrics(&path).await.expect(&format!("No results for {}", path));
+        tokio::pin!(result);
+    
+        while let Some(lyric) = result.next().await {
+            println!("id: {}", lyric.id);
+            println!("Number of parts: {}", lyric.parts.len());
+         
+            if let Some(title) = lyric.title {
+                println!("Title: {}", title);
+            }
+            if let Some(member_of) = lyric.member_of {
+                println!("Member of: {}", member_of.join(", "));
+            }
+            println!();
+        }
+    
+        println!("Elapsed: {} ms", start.elapsed().as_millis());
+        Ok(())
+    })
 }
