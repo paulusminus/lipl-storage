4cdf699b34ca8ce19e5aee99e4da468d3afde0bc
Paul Min <info@paulmin.nl>
2020-11-17 22:08:44 +0100

    Commandline arguments

--- examples/01.rs
+++ examples/01.rs
@@ -1,9 +1,27 @@
-use std::io::{ErrorKind};
 use std::collections::HashMap;
-use lipl_io::{get_lyrics};
+use std::io::{Error as IOError, ErrorKind};
+use std::path::Path;
+use std::time::{Instant};
+
 use tokio::runtime::{Builder};
 use tokio::stream::StreamExt;
-use std::time::{Instant};
+
+use lipl_io::{get_lyrics};
+
+fn get_path() -> Result<String, IOError> {
+    let mut args = std::env::args();
+
+    if args.len() < 2 {
+        return Err(IOError::new(ErrorKind::Other, "Argument directory missing"));
+    }
+
+    let path = args.nth(1).ok_or(std::io::Error::new(ErrorKind::Other, "Cannot parse argument 1"))?;
+    if !Path::new(&path).exists() {
+        return Err(IOError::new(ErrorKind::Other, "Directory not found"));
+    }
+
+    Ok(path)
+}
 
 fn main() -> Result<(), std::io::Error> {
 
@@ -12,40 +30,32 @@ fn main() -> Result<(), std::io::Error> {
     rt.block_on(async {
         let start = Instant::now();
 
-        let mut args = std::env::args();
-        if args.len() < 2 {
-            return Err(std::io::Error::new(ErrorKind::Other, "Argument directory missing"));
-        }
-    
-        let path = args.nth(1).unwrap();
-        if !std::path::Path::new(&path).exists() {
-            return Err(std::io::Error::new(ErrorKind::Other, "Directory not found"));
-        }
-    
+        let path = get_path()?;
         let mut hm: HashMap<String, Vec<String>> = HashMap::new();
-        let result = get_lyrics(&path).await.expect(&format!("No results for {}", path));
-        tokio::pin!(result);
+
+        let lyrics = get_lyrics(&path).await.expect(&format!("No results for {}", &path));
+        tokio::pin!(lyrics);
     
-        while let Some(lyric) = result.next().await {
+        while let Some(lyric) = lyrics.next().await {
             if let Some(playlists) = &lyric.member_of {
                 playlists.iter().for_each(|pl| {
                     hm.entry(pl.clone()).or_insert(Vec::new()).push(lyric.id.clone());
                 });
-            }
+            };
 
             println!(
                 "Title: {}, {} parts, id = {}, member of: {}",
                 lyric.title.unwrap_or("<< onbekend >>".to_owned()),
                 lyric.parts.len(),
                 lyric.id,
-                lyric.member_of.unwrap_or_default().join(", ")
-
+                lyric.member_of.unwrap_or_default().join(", "),
             );
             println!();
         }
 
         hm.keys().for_each(|key| {
-            println!("{}:", key);
+            println!("title: {}", key);
+            println!("members:");
             hm[key].iter().for_each(|value| {
                 println!("  - {}", value);
             });
