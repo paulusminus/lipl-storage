9eaffd159e672d80f3c574233109b377858ec374
Paul Min <info@paulmin.nl>
2020-11-27 22:34:50 +0100

    Filters to separate module

---
+++ src/filter.rs
@@ -0,0 +1,59 @@
+use std::collections::HashMap;
+use std::sync::{Arc, RwLock};
+use warp::{body, path, Filter};
+use lipl_io::{Uuid, Deserialize, Serialize};
+use lipl_io::model::{HasId, HasSummary};
+use super::handler;
+
+const VERSION: &str = "v1";
+
+pub fn get_routes<T, U>(db: HashMap<Uuid, T>, name: &'static str) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
+where T: From<U> + HasSummary + HasId + Serialize + Clone + Send + Sync,
+U: for<'de> Deserialize<'de> + Send {
+    let arc = Arc::new(RwLock::new(db));
+    let db  = warp::any().map(move || arc.clone());
+
+    let list = 
+        warp::get()
+        .and(path(VERSION))
+        .and(path(name))
+        .and(path::end())
+        .and(db.clone())
+        .and_then(handler::list);
+
+    let item =
+        warp::get()
+        .and(path(VERSION))
+        .and(path(name))
+        .and(path::param())
+        .and(db.clone())
+        .and_then(handler::item);
+
+    let post =
+        warp::post()
+        .and(path(VERSION))
+        .and(path(name))
+        .and(path::end())
+        .and(body::json::<U>())
+        .and(db.clone())
+        .and_then(handler::post);
+
+    let delete =
+        warp::delete()
+        .and(warp::path(VERSION))
+        .and(warp::path(name))
+        .and(warp::path::param())
+        .and(db.clone())
+        .and_then(handler::delete);
+
+    let put =
+        warp::put()
+        .and(path(VERSION))
+        .and(path(name))
+        .and(path::param())
+        .and(body::json::<U>())
+        .and(db.clone())
+        .and_then(handler::put);
+
+    list.or(item).or(post).or(put).or(delete)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,71 +1,18 @@
 #[macro_use]
 extern crate log;
 
-use std::collections::HashMap;
-use std::sync::{Arc, RwLock};
-use warp::{body, path, Filter};
-use lipl_io::{Uuid, Deserialize, Serialize};
-use lipl_io::model::{create_db, HasId, HasSummary, LyricPost, PlaylistPost, Lyric, Playlist};
-
+mod filter;
 mod handler;
 mod param;
 
-const VERSION: &str = "v1";
-const LYRIC: &str = "lyric";
-const PLAYLIST: &str = "playlist";
+use warp::Filter;
+use lipl_io::model::{create_db, LyricPost, PlaylistPost, Lyric, Playlist};
+use filter::get_routes;
+
 const PORT: u16 = 3030;
 const HOST: [u8; 4] = [0, 0, 0, 0];
-
-fn get_routes<T, U>(db: HashMap<Uuid, T>, name: &'static str) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone
-where T: From<U> + HasSummary + HasId + Serialize + Clone + Send + Sync,
-U: for<'de> Deserialize<'de> + Send {
-    let arc = Arc::new(RwLock::new(db));
-    let db  = warp::any().map(move || arc.clone());
-
-    let list = 
-        warp::get()
-        .and(path(VERSION))
-        .and(path(name))
-        .and(path::end())
-        .and(db.clone())
-        .and_then(handler::list);
-
-    let item =
-        warp::get()
-        .and(path(VERSION))
-        .and(path(name))
-        .and(path::param())
-        .and(db.clone())
-        .and_then(handler::item);
-
-    let post =
-        warp::post()
-        .and(path(VERSION))
-        .and(path(name))
-        .and(path::end())
-        .and(body::json::<U>())
-        .and(db.clone())
-        .and_then(handler::post);
-
-    let delete =
-        warp::delete()
-        .and(warp::path(VERSION))
-        .and(warp::path(name))
-        .and(warp::path::param())
-        .and(db.clone())
-        .and_then(handler::delete);
-
-    let put =
-        warp::put()
-        .and(path(VERSION))
-        .and(path(name))
-        .and(path::param())
-        .and(body::json::<U>())
-        .and(db.clone())
-        .and_then(handler::put);
-
-    list.or(item).or(post).or(put).or(delete)
-}
+const LYRIC: &str = "lyric";
+const PLAYLIST: &str = "playlist";
 
 #[tokio::main]
 async fn main() -> tokio::io::Result<()> {
