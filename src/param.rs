7829e96e5ff822bf7f0efe3690b2b47aedfa9736
Paul Min <info@paulmin.nl>
2020-12-17 09:00:50 +0100

    Test for zip file or directory changed. fs::meta

--- src/param.rs
+++ src/param.rs
@@ -2,6 +2,7 @@ use std::path::{PathBuf};
 use clap::{Clap, ValueHint};
 
 #[derive(Clap, Debug)]
+#[clap(about = "Serving the db through http")]
 pub struct Serve {
     #[clap(short, long, required = true)]
     pub port: u16,
@@ -10,12 +11,14 @@ pub struct Serve {
 }
 
 #[derive(Clap, Debug)]
+#[clap(about = "Show db summary on console")]
 pub struct ListCommand {
     #[clap(name = "source", parse(from_os_str), value_hint = ValueHint::AnyPath)]
     pub source: PathBuf,
 }
 
 #[derive(Clap, Debug)]
+#[clap(about = "Copy db to another destination")]
 pub struct CopyCommand {
     #[clap(name = "source", parse(from_os_str), value_hint = ValueHint::AnyPath)]
     pub source: PathBuf,
@@ -24,6 +27,7 @@ pub struct CopyCommand {
 }
 
 #[derive(Clap, Debug)]
+#[clap(about = "Utilities for db")]
 pub enum DbCommand {
     #[clap(name = "list")]
     List(ListCommand),
