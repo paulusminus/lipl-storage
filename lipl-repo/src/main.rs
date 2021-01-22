e480f313cd6c296a3be865a651a2ad2fb2e2c0e0
Paul Min <info@paulmin.nl>
2021-01-22 08:35:56 +0100

    Upgrade dependencies

--- lipl-repo/src/main.rs
+++ lipl-repo/src/main.rs
@@ -15,7 +15,7 @@ mod serve;
 use anyhow::Result;
 use clap::Clap;
 
-#[tokio::main]
+#[tokio::main(flavor = "current_thread") ]
 async fn main() -> Result<()> {
     env_logger::Builder::from_env(env_logger::Env::default().default_filter_or(constant::LOG_LEVEL)).init();
     info!("{}", message::STARTING);
@@ -23,21 +23,19 @@ async fn main() -> Result<()> {
     let arguments = param::Arguments::parse();
     match arguments.command {
         param::Command::Serve(serve_args) => {
-            serve::serve(serve_args).await?;
+            serve::serve(serve_args).await
         },
         param::Command::Db(db) => {
             match db {
                 param::DbCommand::Copy(copy_args) => {
-                    db::copy(copy_args)?;
+                    db::copy(copy_args)
                 },
                 param::DbCommand::List(list_args) => {
-                    db::list(list_args)?;
+                    db::list(list_args)
                 },
             }
         }
     }
-
-    Ok(())
 }
 
 #[cfg(test)]
