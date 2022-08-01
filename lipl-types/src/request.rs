c5dda57b25dc1d9e302988ac45df52dee3e63b65
Paul Min <info@paulmin.nl>
2022-08-01 17:27:01 +0200

    Refactoring

--- lipl-types/src/request.rs
+++ lipl-types/src/request.rs
@@ -1,22 +1,24 @@
-use crate::{Uuid, Summary, LyricPost, PlaylistPost, Lyric, Playlist, RepoResult, RepoError};
+use crate::{Uuid, Summary, LyricPost, PlaylistPost, Lyric, Playlist, RepoResult};
 use futures::channel::{mpsc, oneshot};
 
+type ResultSender<T> = oneshot::Sender<RepoResult<T>>;
+
 pub enum Request {
-    LyricSummaries(oneshot::Sender<RepoResult<Vec<Summary>>>),
-    LyricList(oneshot::Sender<RepoResult<Vec<Lyric>>>),
-    LyricItem(Uuid, oneshot::Sender<RepoResult<Lyric>>),
-    LyricDelete(Uuid, oneshot::Sender<RepoResult<()>>),
-    LyricPost(LyricPost, oneshot::Sender<RepoResult<()>>),
-    LyricPut(Uuid, LyricPost, oneshot::Sender<RepoResult<()>>),
-    PlaylistSummaries(oneshot::Sender<RepoResult<Vec<Summary>>>),
-    PlaylistList(oneshot::Sender<RepoResult<Vec<Playlist>>>),
-    PlaylistItem(Uuid, oneshot::Sender<RepoResult<Playlist>>),
-    PlaylistDelete(Uuid, oneshot::Sender<RepoResult<()>>),
-    PlaylistPost(PlaylistPost, oneshot::Sender<RepoResult<()>>),
-    PlaylistPut(Uuid, PlaylistPost, oneshot::Sender<RepoResult<()>>),
+    LyricSummaries(ResultSender<Vec<Summary>>),
+    LyricList(ResultSender<Vec<Lyric>>),
+    LyricItem(Uuid, ResultSender<Lyric>),
+    LyricDelete(Uuid, ResultSender<()>),
+    LyricPost(LyricPost, ResultSender<()>),
+    LyricPut(Uuid, LyricPost, ResultSender<()>),
+    PlaylistSummaries(ResultSender<Vec<Summary>>),
+    PlaylistList(ResultSender<Vec<Playlist>>),
+    PlaylistItem(Uuid, ResultSender<Playlist>),
+    PlaylistDelete(Uuid, ResultSender<()>),
+    PlaylistPost(PlaylistPost, ResultSender<()>),
+    PlaylistPut(Uuid, PlaylistPost, ResultSender<()>),
 }
 
-pub async fn send<T>(tx: &mut mpsc::Sender<Request>, f: impl Fn(oneshot::Sender<Result<T, RepoError>>) -> Request) -> RepoResult<T> {
+pub async fn send<T>(tx: &mut mpsc::Sender<Request>, f: impl Fn(ResultSender<T>) -> Request) -> RepoResult<T> {
     let (oneshot_tx, oneshot_rx) = oneshot::channel::<RepoResult<T>>();
     tx.try_send(f(oneshot_tx))?;
     oneshot_rx.await?
