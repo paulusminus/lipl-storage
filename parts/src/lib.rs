450ff82d79e23cd37e834bf94e1b82f9afb95782
Paul Min <info@paulmin.nl>
2022-08-07 08:33:12 +0200

    include parts

---
+++ parts/src/lib.rs
@@ -0,0 +1,88 @@
+use regex::Regex;
+use std::fmt::{Display, Formatter, Result};
+
+mod st;
+pub use st::to_parts_async;
+
+struct Parts(Vec<Vec<String>>);
+
+impl From<String> for Parts {
+    fn from(s: String) -> Self {
+        Parts(to_parts(s))
+    }
+}
+
+impl Display for Parts {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        write!(f, "{}", to_text(&self.0))
+    }
+}
+
+fn trim(s: &str) -> &str {
+    s.trim()
+}
+
+fn to_lines(s: &str) -> Vec<String> {
+    s.split('\n')
+        .map(trim)
+        .filter(|&s| !s.is_empty())
+        .map(|s| s.to_string())
+        .collect()
+}
+
+pub fn to_parts(s: String) -> Vec<Vec<String>> {
+    Regex::new(r"\n\s*\n")
+        .unwrap()
+        .split(&s)
+        .map(to_lines)
+        .filter(|p| !p.is_empty())
+        .collect()
+}
+
+pub fn to_text(parts: &[Vec<String>]) -> String {
+    parts
+    .iter()
+    .map(|lines| lines.join("\n"))
+    .collect::<Vec<String>>()
+    .join("\n\n")
+}
+
+#[cfg(test)]
+mod test {
+    use super::{to_lines, to_parts, trim, Parts};
+
+    #[test]
+    fn test_trim() {
+        let test = "\rHallo allema  \t";
+        assert_eq!(trim(test), "Hallo allema");
+    }
+
+    #[test]
+    fn test_to_lines() {
+        let test = "Hallo\nAllemaal\r\nWat fijn  \n\r\n";
+        let result = to_lines(test);
+        assert_eq!(result.len(), 3);
+        assert_eq!(result[0], "Hallo");
+        assert_eq!(result[1], "Allemaal");
+        assert_eq!(result[2], "Wat fijn");
+    }
+
+    #[test]
+    fn test_to_parts() {
+        let test = "Hallo\nAllemaal\r\n\nWat fijn  \n\r\n".to_owned();
+        let result = to_parts(test);
+        assert_eq!(result.len(), 2);
+        assert_eq!(&result[0][0], "Hallo");
+        assert_eq!(&result[0][1], "Allemaal");
+        assert_eq!(result[0].len(), 2);
+        assert_eq!(&result[1][0], "Wat fijn");
+        assert_eq!(result[1].len(), 1);
+    }
+
+    #[test]
+    fn test_to_text() {
+        let test = "Hallo\nAllemaal\r\n\nWat fijn  \n\r\n".to_owned();
+        let result = Parts::from(test).to_string();
+        assert_eq!(result, "Hallo\nAllemaal\n\nWat fijn");
+    }
+}
---
+++ parts/src/st.rs
@@ -0,0 +1,54 @@
+use futures::Stream;
+use futures::stream::StreamExt;
+use std::io::Error;
+
+pub async fn to_parts_async(mut s: impl Stream<Item=Result<String, Error>> + Unpin) -> Result<Vec<Vec<String>>, Error>
+{
+    let mut new_part = true;
+    let mut result: Vec<Vec<String>> = vec![];
+    while let Some(line) = s.next().await {
+        let trimmed: String = line?.trim().into();
+        if trimmed.is_empty() {
+            new_part = true;
+        }
+        else if new_part {
+            result.push(vec![trimmed]);
+            new_part = false;
+        }
+        else if let Some(last) = result.last_mut() {
+            last.push(trimmed);
+        }
+    }
+    Ok(result)
+}
+
+#[cfg(test)]
+mod test {
+    use futures::io::{AsyncBufReadExt, BufReader, Cursor, Error};
+    use futures::{Stream};
+    use crate::st::to_parts_async;
+
+    fn get_data() -> impl Stream<Item=Result<String, Error>> + Unpin {
+        const BUFFER: &[u8] = b"Hallo allemaal\r\n  Wat fijn dat u er bent\t\n\n En dan ook nog\neen tweede couplet";
+        let cursor = Cursor::new(BUFFER);
+        let line_stream = BufReader::new(cursor).lines();
+        line_stream
+    }
+
+    #[tokio::test]
+    async fn test_async_to_parts() {
+        assert_eq!(
+            to_parts_async(&mut get_data()).await.unwrap(),
+            vec![
+                vec![
+                    "Hallo allemaal".to_owned(),
+                    "Wat fijn dat u er bent".to_owned(),
+                ],
+                vec![
+                    "En dan ook nog".to_owned(),
+                    "een tweede couplet".to_owned()
+                ]
+            ],
+        );
+    }
+}
