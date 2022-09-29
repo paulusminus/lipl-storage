08aac90b09da9c0d7fe0ae9b80fc2ae66101926a
Paul Min <info@paulmin.nl>
2022-09-29 17:00:13 +0200

    Added documentation for timeit

--- timeit-test/src/main.rs
+++ timeit-test/src/main.rs
@@ -1,20 +1,24 @@
 use timeit::timeit;
-use tracing::instrument;
 use tracing_subscriber::filter::LevelFilter;
-use std::error::Error;
+use std::{error::Error, path::Path, fmt::Debug};
 
 type Result<T> = std::result::Result<T, Box<dyn Error>>;
+type IOResult<T> = std::result::Result<T, std::io::Error>;
 
-#[instrument]
+const FILE: &str = ".gitignore";
+
 #[timeit]
-pub async fn jaja(hallo: &str) -> Result<String> {
-    Ok(hallo.into())
+pub async fn read_file<P>(path: P) -> IOResult<String>
+where P: AsRef<Path> + Debug
+{
+    tokio::fs::read_to_string(path).await
 }
 
 #[tokio::main(flavor = "current_thread")]
 async fn main() -> Result<()> {
-    tracing_subscriber::fmt().with_max_level(LevelFilter::INFO).init();
-    let result = jaja("hallo").await?;
-    println!("Hello, {result}");
+    tracing_subscriber::fmt().with_max_level(LevelFilter::TRACE).init();
+    let result = read_file(FILE).await?;
+    println!("File: {FILE}");
+    println!("{result}");
     Ok(())
 }
--- timeit/src/lib.rs
+++ timeit/src/lib.rs
@@ -1,24 +1,67 @@
 use proc_macro::{TokenStream};
 use syn::{parse_macro_input, ItemFn};
-use quote::quote;
+use quote::{quote};
 
+const ASYNC_MISSING_MSG: &str = "#[time_it]: the `async` keyword is missing from the function declaration";
+
+fn token_stream_with_error(mut tokens: TokenStream, error: syn::Error) -> TokenStream {
+    tokens.extend(TokenStream::from(error.into_compile_error()));
+    tokens
+}
+
+
+/// Marks async function to report the time it took to execute by means of tracing.
+/// 
+/// # Example
+/// ```rust
+/// use timeit::timeit;
+/// 
+/// #[timeit]
+/// async fn test() -> Result<String, std::io::Error> {
+///     tokio::fs::read_to_string("Cargo.toml").await
+/// }
+/// ```
+/// 
+/// this example is equivalent to the following code
+/// 
+/// ```rust
+/// #[tracing::instrument]
+/// async fn test() -> Result<String, std::io::Error> {
+///     let now = std::time::Instant::now();
+///     let result = async move { tokio::fs::read_to_string("Cargo.toml").await }.await;
+///     ::tracing::trace!("Function execution took {:?}", now.elapsed());
+///     result
+/// }
+/// ```
 #[proc_macro_attribute]
 pub fn timeit(_args: TokenStream, input: TokenStream) -> TokenStream {
-    let func = parse_macro_input!(input as ItemFn);
+    let fn_tokens = input.clone();
+    let func = parse_macro_input!(fn_tokens as ItemFn);
 
-    let vis = func.vis.clone();
-    let sig = func.sig.clone();
-    let block = func.block;
 
-    let expanded = quote! {
-        #vis #sig {
-            let now = ::std::time::Instant::now();
-            let result = async move #block.await;
-            let elapsed_seconds = now.elapsed().as_secs();
-            ::tracing::info!("{} seconds elapsed", elapsed_seconds);
-            result
+    if func.sig.asyncness.is_none() {
+        token_stream_with_error(
+            input,
+            syn::Error::new_spanned(
+                func.sig.fn_token,
+                ASYNC_MISSING_MSG,
+            ),
+        )
+    }
+    else {
+        let vis = func.vis.clone();
+        let sig = func.sig.clone();
+        let block = func.block;
+        quote! {
+            #[::tracing::instrument]
+            #vis #sig {
+                let now = ::std::time::Instant::now();
+                let result = async move #block.await;
+                ::tracing::trace!("function execution took {:?}", now.elapsed());
+                result
+            }
         }
-    };
-    expanded.into()
+        .into()    
+    }
 }
 
