146dbbab5bb9e67e257fe867f53acced29e0664d
Paul Min <info@paulmin.nl>
2022-09-29 20:36:40 +0200

    Documentation finished for timeit

---
+++ timeit/README.md
@@ -0,0 +1,13 @@
+# Timeit attribute macro
+
+Procedural attribute macro to track the time need for execution a async function
+
+## License
+
+This project is licensed under the [MIT license](LICENSE).
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in Tokio by you, shall be licensed as MIT, without any additional
+terms or conditions.
--- timeit/src/lib.rs
+++ timeit/src/lib.rs
@@ -1,43 +1,78 @@
-use proc_macro::{TokenStream};
-use syn::{parse_macro_input, ItemFn};
-use quote::{quote};
+#![allow(clippy::needless_doctest_main)]
+#![warn(
+    missing_debug_implementations,
+    missing_docs,
+    rust_2018_idioms,
+    unreachable_pub
+)]
+#![doc(
+    test(
+        no_crate_inject,
+        attr(
+            deny(
+                warnings,
+                rust_2018_idioms
+            ),
+            allow(
+                dead_code,
+                unused_variables
+            )
+    )
+))]
 
+//! Attribute Procedural Macro for tracing elapsed time
+
+use proc_macro::TokenStream;
+use syn::{parse_macro_input, ItemFn};
+use quote::quote;
+
+// Error message when annotated function is not async
 const ASYNC_MISSING_MSG: &str = "#[time_it]: the `async` keyword is missing from the function declaration";
 
+// Report error to the compiler
 fn token_stream_with_error(mut tokens: TokenStream, error: syn::Error) -> TokenStream {
     tokens.extend(TokenStream::from(error.into_compile_error()));
     tokens
 }
 
-
 /// Marks async function to report the time it took to execute by means of tracing.
 /// 
 /// # Example
-/// ```rust
+/// 
+/// ```
 /// use timeit::timeit;
+/// use tokio::fs::read_to_string;
+/// use tokio_test::block_on;
 /// 
 /// #[timeit]
 /// async fn test() -> Result<String, std::io::Error> {
-///     tokio::fs::read_to_string("Cargo.toml").await
+///     read_to_string("Cargo.toml").await
 /// }
+/// 
+/// assert!(block_on(async { test().await }).is_ok());
 /// ```
 /// 
 /// this example is equivalent to the following code
 /// 
-/// ```rust
-/// #[tracing::instrument]
+/// ```
+/// use tracing::{instrument, trace};
+/// use tokio::fs::read_to_string;
+/// use tokio_test::block_on;
+/// 
+/// #[instrument]
 /// async fn test() -> Result<String, std::io::Error> {
 ///     let now = std::time::Instant::now();
-///     let result = async move { tokio::fs::read_to_string("Cargo.toml").await }.await;
-///     ::tracing::trace!("Function execution took {:?}", now.elapsed());
+///     let result = async move { read_to_string("Cargo.toml").await }.await;
+///     trace!("Function execution took {:?}", now.elapsed());
 ///     result
 /// }
+/// 
+/// assert!(block_on(async { test().await }).is_ok());
 /// ```
 #[proc_macro_attribute]
 pub fn timeit(_args: TokenStream, input: TokenStream) -> TokenStream {
     let fn_tokens = input.clone();
     let func = parse_macro_input!(fn_tokens as ItemFn);
-
 
     if func.sig.asyncness.is_none() {
         token_stream_with_error(
@@ -64,4 +99,3 @@ pub fn timeit(_args: TokenStream, input: TokenStream) -> TokenStream {
         .into()    
     }
 }
-
