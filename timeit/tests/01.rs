8fee5c51f763ab6bcceef8994f2a2f216f3a9711
Paul Min <info@paulmin.nl>
2022-09-30 12:22:52 +0200

    Integration test for timeit

---
+++ timeit/tests/01.rs
@@ -0,0 +1,17 @@
+use timeit::timeit;
+
+#[timeit]
+async fn get_str() -> &'static str {
+    futures::future::ready("Cargo.toml").await
+}
+
+#[test]
+fn testen_maar() {
+    tracing_subscriber::fmt()
+    .with_test_writer()
+    .with_max_level(tracing::Level::TRACE)
+    .init();
+
+    let result = futures::executor::block_on(get_str());
+    assert_eq!(result, "Cargo.toml");
+}
