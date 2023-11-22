30bd9309ef95469c2274fdede6b943a297659a97
paulusminus <info@paulmin.nl>
2023-11-22 14:56:35 +0100

    prepare for github actions build

--- run-fs-local.sh
+++ run-fs-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LIPL_STORAGE_REPO_TYPE=fs
-export LIPL_STORAGE_FS_DIR="./data_lipl/"
+export LIPL_STORAGE_FS_DIR="./data/"
 
 ./target/x86_64-unknown-linux-musl/release/lipl-storage-fs
--- run-postgres-local.sh
+++ run-postgres-local.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 export LIPL_STORAGE_REPO_TYPE=postgres
-export LIPL_STORAGE_POSTGRES_CONNECTION="host=/run/postgresql/ dbname=test2"
+export LIPL_STORAGE_POSTGRES_CONNECTION=host=/run/postgresql/ dbname=test2
 
 ./target/x86_64-unknown-linux-musl/release/lipl-storage-postgres
