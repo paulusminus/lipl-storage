c4a951885ac69b46c1ce4e88dadfb06eefdcabea
paulusminus <info@paulmin.nl>
2024-04-30 10:44:51 +0200

    Backup converted file

--- crates/lipl-storage-fs/src/bin/convert.rs
+++ crates/lipl-storage-fs/src/bin/convert.rs
@@ -43,36 +43,59 @@ fn write_object<D: std::fmt::Display, P: AsRef<Path>>(out_path: P, d: D) -> Resu
     Ok(())
 }
 
+fn is_file(de: &DirEntry) -> bool {
+    de.file_type().ok().map(|f| f.is_file()) == Some(true)   
+}
+
+fn backup(de: &DirEntry) -> Result<()> {
+    let mut backup = de.path().as_os_str().to_owned();
+    backup.push(".bak");
+    rename(de.path(), backup)?;
+    Ok(())
+}
+
 fn main() -> Result<()> {
     let in_dir = var("LIPL_STORAGE_FS_DIR").map(PathBuf::from)?;
 
-    if !in_dir.is_dir() {
+    if in_dir.exists() && in_dir.is_dir() {
+        println!("Using {} as input directory", in_dir.to_string_lossy());
+    } else {
         return Err(Error::NonExistingDirectory(in_dir));
     }
 
-    if !in_dir.join(TO_TOML_OK_FILENAME).exists() {
-        let dir = read_dir(&in_dir)?;
-        let files = dir.map_while(|r| r.ok());
-
-        for file in files.filter(|de| de.file_type().unwrap().is_file()) {
-            if file.path().extension() == Some(OsStr::new(YAML_EXTENSION)) {
-                let playlist = to_object::<PlaylistPostWrapper, Playlist>(&file)?;
-                let out_path = in_dir.join(format!("{}.{}", playlist.id, TOML_EXTENSION));
-                rename(file.path(), file.path().join(".bak"))?;
-                write_object(&out_path, playlist)?;
-            } else if file.path().extension() == Some(OsStr::new(LYRIC_EXTENSION)) {
-                let lyric = to_object::<LyricPostWrapper, Lyric>(&file)?;
-                let out_path = in_dir.join(format!("{}.{}", lyric.id, LYRIC_EXTENSION));
-                rename(file.path(), file.path().join(".bak"))?;
-                write_object(out_path, lyric)?;
-            }
-        }
-        OpenOptions::new()
-            .create(true)
-            .truncate(false)
-            .write(true)
-            .open(in_dir.join(TO_TOML_OK_FILENAME))?;
+    if in_dir.join(TO_TOML_OK_FILENAME).exists() && in_dir.join(TO_TOML_OK_FILENAME).is_file() {
+        return Err(Error::Argument("Already converted"));
     }
+
+    let dir = read_dir(&in_dir)?;
+    let files = dir.map_while(|r| r.ok());
+
+    for file in files.filter(is_file) {
+        println!("Processing file {}", file.file_name().to_string_lossy());
+        if file.path().extension() == Some(OsStr::new(YAML_EXTENSION)) {
+            let playlist = to_object::<PlaylistPostWrapper, Playlist>(&file)?;
+            println!("Found playlist with title {}", playlist.title);
+            let out_path = in_dir.join(format!("{}.{}", playlist.id, TOML_EXTENSION));
+
+            backup(&file)?;
+            println!("original file renamed with bak extension");
+
+            write_object(&out_path, playlist)?;
+        } else if file.path().extension() == Some(OsStr::new(LYRIC_EXTENSION)) {
+            let lyric = to_object::<LyricPostWrapper, Lyric>(&file)?;
+            println!("Found lyric with title {}", lyric.title);
+            let out_path = in_dir.join(format!("{}.{}", lyric.id, LYRIC_EXTENSION));
+
+            backup(&file)?;
+            println!("original file renamed with bak extension");
+            write_object(out_path, lyric)?;
+        }
+    }
+    OpenOptions::new()
+        .create(true)
+        .truncate(false)
+        .write(true)
+        .open(in_dir.join(TO_TOML_OK_FILENAME))?;
 
     Ok(())
 }
