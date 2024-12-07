99c512fc9d732418a47aef719564416196b2cd5f
paulusminus <info@paulmin.nl>
2024-12-07 22:16:13 +0100

    chore: update dependencies

--- crates/hurl-runner/src/main.rs
+++ crates/hurl-runner/src/main.rs
@@ -1,24 +1,26 @@
 use hurl::{
     output::write_last_body,
-    runner::{run, HurlResult, RunnerOptions, RunnerOptionsBuilder, Value},
+    runner::{run, HurlResult, RunnerOptions, RunnerOptionsBuilder, Value, VariableSet},
     util::{
         logger::{LoggerOptions, LoggerOptionsBuilder},
         term::{Stdout, WriteMode},
     },
 };
-use std::{collections::HashMap, env::var};
+use std::env::var;
 
 type Result<T, E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 mod constant;
 
-fn variables() -> HashMap<String, Value> {
-    let mut variables = HashMap::<String, Value>::new();
-    variables.insert(
-        constant::PREFIX_NAME.into(),
-        Value::String(constant::PREFIX_VALUE.into()),
-    );
+fn variableset() -> Result<VariableSet> {
+    let mut variables = VariableSet::new();
     variables
+        .insert(
+            constant::PREFIX_NAME.into(),
+            Value::String(constant::PREFIX_VALUE.into()),
+        )
+        .map_err(|_| "Cannot insert key value into variableset".to_owned())?;
+    Ok(variables)
 }
 
 fn logger_options() -> LoggerOptions {
@@ -36,7 +38,7 @@ fn user() -> Result<String> {
 
 fn run_with_options(
     script: &str,
-    variables: &HashMap<String, Value>,
+    variables: &VariableSet,
     runner_options: &RunnerOptions,
     logger_options: &LoggerOptions,
 ) -> Result<HurlResult> {
@@ -52,7 +54,7 @@ fn handle_output(out: &mut Stdout) -> impl FnMut(HurlResult) -> Result<()> + '_ 
 
 fn run_script<'a>(
     out: &'a mut Stdout,
-    variables: &'a HashMap<String, Value>,
+    variables: &'a VariableSet,
     runner_options: &'a RunnerOptions,
     logger_options: &'a LoggerOptions,
 ) -> impl FnMut(&str) -> Result<()> + 'a {
@@ -66,7 +68,7 @@ fn main() -> Result<()> {
     let out = &mut Stdout::new(WriteMode::Immediate);
     let user = user()?;
     let runner_options = RunnerOptionsBuilder::new().user(Some(user)).build();
-    let variables = variables();
+    let variables = variableset()?;
     let logger_options = logger_options();
 
     constant::HURL_SCRIPTS.into_iter().try_for_each(run_script(
