271558d3d53f1d09546f5a7b7bbd58915a8495f2
paulusminus <info@paulmin.nl>
2025-03-21 09:37:59 +0100

    chore: update dependencies

--- crates/lipl-storage-fs/src/request.rs
+++ crates/lipl-storage-fs/src/request.rs
@@ -2,7 +2,7 @@ use crate::Uuid;
 use futures_channel::oneshot::Canceled;
 use futures_channel::{mpsc, oneshot};
 use lipl_core::Error;
-use lipl_core::transaction::{Request, ResultSender};
+use lipl_core::transaction::Request;
 use std::fmt::Debug;
 
 type Result<T> = std::result::Result<T, Error>;
@@ -17,7 +17,7 @@ fn canceled(error: Canceled) -> Error {
 
 pub async fn select<T>(
     mut tx: mpsc::Sender<Request>,
-    f: fn(ResultSender<T>) -> Request,
+    f: fn(oneshot::Sender<Result<T>>) -> Request,
 ) -> Result<T> {
     let (oneshot_tx, oneshot_rx) = oneshot::channel::<Result<T>>();
     tx.try_send(f(oneshot_tx)).map_err(send_failed)?;
@@ -27,7 +27,7 @@ pub async fn select<T>(
 pub async fn select_by_id<T>(
     mut tx: mpsc::Sender<Request>,
     uuid: Uuid,
-    f: fn(Uuid, ResultSender<T>) -> Request,
+    f: fn(Uuid, oneshot::Sender<Result<T>>) -> Request,
 ) -> Result<T> {
     let (oneshot_tx, oneshot_rx) = oneshot::channel::<Result<T>>();
     tx.try_send(f(uuid, oneshot_tx)).map_err(send_failed)?;
@@ -37,7 +37,7 @@ pub async fn select_by_id<T>(
 pub async fn delete_by_id(
     mut tx: mpsc::Sender<Request>,
     uuid: Uuid,
-    f: fn(Uuid, ResultSender<()>) -> Request,
+    f: fn(Uuid, oneshot::Sender<Result<()>>) -> Request,
 ) -> Result<()> {
     let (oneshot_tx, oneshot_rx) = oneshot::channel::<Result<()>>();
     tx.try_send(f(uuid, oneshot_tx)).map_err(send_failed)?;
@@ -47,7 +47,7 @@ pub async fn delete_by_id(
 pub async fn post<T: Debug>(
     mut tx: mpsc::Sender<Request>,
     t: T,
-    f: fn(T, ResultSender<T>) -> Request,
+    f: fn(T, oneshot::Sender<Result<T>>) -> Request,
 ) -> Result<T> {
     let (oneshot_tx, oneshot_rx) = oneshot::channel::<Result<T>>();
     tx.try_send(f(t, oneshot_tx)).map_err(send_failed)?;
