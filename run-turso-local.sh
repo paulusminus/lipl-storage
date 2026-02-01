d11c82fca720cdb16288f584dc3e833680a3a63d
Paul Min <info@paulmin.nl>
2026-02-01 08:14:58 +0100

    chore: fix musl build

--- run-turso-local.sh
+++ run-turso-local.sh
@@ -3,4 +3,4 @@
 export LIPL_STORAGE_REPO_TYPE=turso
 export LIPL_STORAGE_TURSO_DATABASE_PATH=lipl.sqlite
 
-./target/x86_64-unknown-linux-gnu/release/lipl-storage-server
+./target/x86_64-unknown-linux-musl/release/lipl-storage-server
