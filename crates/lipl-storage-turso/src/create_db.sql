ad8418a075ad28018a85df69e3622f4b3f961a2e
Paul Min <info@paulmin.nl>
2026-02-01 11:29:33 +0100

    chore: create view for including members in playlist

--- crates/lipl-storage-turso/src/create_db.sql
+++ crates/lipl-storage-turso/src/create_db.sql
@@ -19,3 +19,14 @@ CREATE TABLE IF NOT EXISTS member (
 CREATE INDEX IF NOT EXISTS member_lyric_id ON member (lyric_id);
 
 CREATE INDEX IF NOT EXISTS member_playlist_id ON member (playlist_id, ordering);
+
+CREATE VIEW IF NOT EXISTS playlist_view AS
+SELECT
+    id,
+    title,
+    GROUP_CONCAT(lyric_id) members
+FROM playlist
+LEFT JOIN (SELECT * FROM member ORDER BY ordering)
+ON playlist.id = playlist_id
+GROUP BY playlist.id
+ORDER BY playlist.title;
